bb1c62dd6f52d775453095ac8a705d0dbf9f9be8
rubenf11 <93877345+rubenf11@users.noreply.github.com>
2023-04-15 16:22:48 +0100

    Buttons added on index.php

--- index.php
+++ index.php
@@ -9,5 +9,40 @@
 
 <!DOCTYPE html>
 <html lang="en-US">
-   
+<link rel="stylesheet" href="style_index.css">
+   <head>
+      <title>Index</title>
+   </head>
+
+   <body>
+
+      <div class="buttons">
+        <div class="index">
+            <button type="submit" ><a href = "index.php"><b>Início</b></a></button>
+        </div>
+        <br>
+        <div class="tickets">
+            <button type="submit"><a href="tickets.php">Tickets</a></button>
+        </div>
+        <br>
+        <div class="faqs">
+            <button type="submit"><a href="faqs.php">Faqs</a></button>
+        </div>
+        <br>
+        <div class="profile">
+            <button type="submit"><a href="user_profile.php">Edit profile</a></button>
+        </div>
+        <br>
+        <div class="logout">
+            <button type="submit"><a href="logout.php">Logout</a></button>
+        </div>
+      </div>
+
+      <h1>Welcome!</h1>
+      <br>
+
+      <h2>Active tickets</h2>
+
+   </body>
+
 </html>
