ec90534ce8a294e07beb932813739c02fbb7ed88
rubenf11 <93877345+rubenf11@users.noreply.github.com>
2023-04-19 20:00:09 +0100

    Ticket creation completed I guess

--- create_ticket.php
+++ create_ticket.php
@@ -3,6 +3,25 @@
 session_start();
 require_once('connection.php');
 $db = getDatabaseConnection();
+$valid_ticket = 2;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    $valid_ticket = 1;
+    if(!empty($_POST['subject'] && !empty($_POST['description']))){
+        $uniqueid = uniqid();
+        
+        $client_id = $_SESSION["user_id"];
+        $agent_id = 4;
+        $created_at = date("Y/m/d");
+        $updated_at = NULL;
+    
+        $stmt = $db->prepare('INSERT INTO tickets (department_id, client_id, agent_id, subject, description, status, priority, created_at, updated_at) VALUES (?,?,?,?,?, "open",?,?,?)');
+        $stmt->execute(array($_POST["department"],$client_id,$agent_id, $_POST["subject"], $_POST["description"], $_POST["priority"], $created_at, $updated_at));
+    }
+    else{
+        $valid_ticket = 0;
+    }
+}
 
 $stmt = $db->prepare('SELECT * FROM department');
 $stmt->execute();
@@ -15,10 +34,13 @@ $departments = $stmt->fetchAll();
 </head>
 <body>
     <header>
+        <div class="back">
+            <button type="submit"><a href="tickets.php">Back</a></button>
+        </div>
         <div class="title">
             <h2>Create a ticket</h2>
         </div>
-        <form action="tickets_model.php" method="POST">
+        <form method="POST">
             <label for="subject">Subject</label><br><br>
             <textarea id="subject" name="subject" rows="1" cols="50"></textarea>
             <br><br>
@@ -47,9 +69,14 @@ $departments = $stmt->fetchAll();
             <br>       
             <input type="submit" value="Submit">
             <?php
-                if(!$valid_ticket):
+                if($valid_ticket == 0):
             ?>
             <em>Invalid ticket!</em>
+            <?php endif; ?>
+            <?php
+                if($valid_ticket == 1):
+            ?>
+            <em>Ticket created successfully!</em>
             <?php endif; ?>
         </form>
 
